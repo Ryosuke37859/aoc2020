7e94b04cbcf0da7d4734f53cb78fa2ed0c160f6c
Levon Tarver <levon@createthree.com>
2020-12-14 23:33:44 -0600

    Solve Day 4 Part 2

--- lib/aoc2020/day4.rb
+++ lib/aoc2020/day4.rb
@@ -2,25 +2,28 @@
 
 class Aoc2020
   class Day4
+    CM_REGEX = /^(\d+)cm$/.freeze
+    HCL_REGEX = /^#[a-f0-9]{6}$/.freeze
+    IN_REGEX = /^(\d+)in$/.freeze
+    PID_REGEX = /^\d{9}$/.freeze
+    REQUIRED_FIELDS = %i[ecl pid eyr hcl byr iyr hgt].freeze
+    VALID_ECL = %w[amb blu brn gry grn hzl oth].freeze
+
     def initialize(input)
       @data = format_data(input)
-      @valid_passports = []
-      @invalid_passports = []
     end
 
     def part_one
-      required_fields = %i[ecl pid eyr hcl byr iyr hgt]
-      @data.each do |passport_data|
-        if fields_present?(required_fields, passport_data)
-          @valid_passports << passport_data
-        else
-          @invalid_passports << passport_data
-        end
-      end
-      @valid_passports.count
+      @data.map do |passport_data|
+        passport_data if fields_present?(passport_data)
+      end.compact.count
     end
 
-    def part_two; end
+    def part_two
+      @data.map do |passport_data|
+        passport_data if fully_valid?(passport_data)
+      end.compact.count
+    end
 
     private
 
@@ -30,14 +33,73 @@ class Aoc2020
       end
     end
 
+    def fully_valid?(data)
+      return false unless fields_present?(data)
+
+      REQUIRED_FIELDS.each do |field|
+        return false unless send("valid_#{field}?", data[field])
+      end
+      true
+    end
+
     def parse_passport_data(unformatted_passport)
       unformatted_passport.split.map do |pair|
         pair.split(':')
       end.to_h.transform_keys(&:to_sym)
     end
 
-    def fields_present?(fields, passport_data)
-      passport_data.keys.&(fields).length == fields.length
+    def fields_present?(passport_data)
+      (REQUIRED_FIELDS - passport_data.keys).empty?
+    end
+
+    def valid_byr?(byr)
+      valid_year?(range: 1920..2002, year: byr)
+    end
+
+    def valid_ecl?(ecl)
+      VALID_ECL.include? ecl
+    end
+
+    def valid_eyr?(eyr)
+      valid_year?(range: 2020..2030, year: eyr)
+    end
+
+    def valid_hcl?(hcl)
+      return false unless hcl.match(HCL_REGEX)
+
+      true
+    end
+
+    def valid_hgt?(hgt)
+      valid = valid_measurement?(CM_REGEX, 150..193, hgt)
+      valid || valid_measurement?(IN_REGEX, 59..76, hgt)
+    end
+
+    def valid_measurement?(unit_regex, range, hgt)
+      measurement = hgt.match(unit_regex)
+      return false unless measurement
+      return false unless range.include? measurement[1].to_i
+
+      true
+    end
+
+    def valid_pid?(pid)
+      return false unless pid.match(PID_REGEX)
+
+      true
+    end
+
+    def valid_iyr?(iyr)
+      valid_year?(range: 2010..2020, year: iyr)
+    end
+
+    def valid_year?(range:, year:)
+      return false unless year.length == 4
+
+      year = year.to_i
+      return false unless range.include? year
+
+      true
     end
   end
 end
--- spec/lib/aoc2020/day4_spec.rb
+++ spec/lib/aoc2020/day4_spec.rb
@@ -19,11 +19,50 @@ RSpec.describe Aoc2020::Day4 do
     TEST_INPUT
   end
 
-  subject { Aoc2020::Day4.new(data) }
+  let(:invalid_passports) do
+    <<-PASSPORTS
+      eyr:1972 cid:100
+      hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
 
-  it 'solves part one' do
-    expect(subject.part_one).to eq(2)
+      iyr:2019
+      hcl:#602927 eyr:1967 hgt:170cm
+      ecl:grn pid:012533040 byr:1946
+
+      hcl:dab227 iyr:2012
+      ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+      hgt:59cm ecl:zzz
+      eyr:2038 hcl:74454a iyr:2023
+      pid:3556412378 byr:2007
+    PASSPORTS
   end
 
-  it 'solves part two'
+  let(:valid_passports) do
+    <<-PASSPORTS
+      pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+      hcl:#623a2f
+
+      eyr:2029 ecl:blu cid:129 byr:1989
+      iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+      hcl:#888785
+      hgt:164cm byr:2001 iyr:2015 cid:88
+      pid:545766238 ecl:hzl
+      eyr:2022
+
+      iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+    PASSPORTS
+  end
+
+  let(:solver) { Aoc2020::Day4.new(data) }
+  it 'solves part one' do
+    expect(solver.part_one).to eq(2)
+  end
+
+  let(:solver_with_invalid_data) { Aoc2020::Day4.new(invalid_passports) }
+  let(:solver_with_valid_data) { Aoc2020::Day4.new(valid_passports) }
+  it 'solves part two' do
+    expect(solver_with_invalid_data.part_two).to eq(0)
+    expect(solver_with_valid_data.part_two).to eq(4)
+  end
 end
