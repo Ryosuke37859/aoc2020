48cc63f58ce52b1530ba76a18a5278820c52e370
Levon Tarver <levon@createthree.com>
2020-12-15 23:56:41 -0600

    Solve Day 6

--- lib/aoc2020.rb
+++ lib/aoc2020.rb
@@ -6,6 +6,7 @@ require_relative 'aoc2020/day2'
 require_relative 'aoc2020/day3'
 require_relative 'aoc2020/day4'
 require_relative 'aoc2020/day5'
+require_relative 'aoc2020/day6'
 
 class Aoc2020
   class Error < StandardError; end
